40c0e37a433aeee4cc675c64b874c94e6112e3fd
Duy Tân Đỗ <taigaaisaka192002@gmail.com>
2023-03-09 08:08:45 +0700

    first commit

---
+++ BigSchool_ThucHanh/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using BigSchool_ThucHanh.Models;
+using BigSchool_ThucHanh.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool_ThucHanh.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList(),
+            };
+            return View(viewModel);
+        }
+    }
+}
